296f07cf56dda12b08452b395e574a1457e4a40b
Pedro Bacchini <pedrobacchini@outlook.com>
2025-08-17 11:28:14 -0400

    create project

---
+++ src/main/java/com/github/pedrobacchini/ExploreSpringBatchMultiDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreSpringBatchMultiDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreSpringBatchMultiDatasourceApplication.class, args);
+	}
+
+}
